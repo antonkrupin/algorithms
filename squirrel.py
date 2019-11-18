0472d875d3f635d6ed17ea0c2f5555152bf4c570
antonkrupin <krupin_anton@mail.ru>
2019-11-18 17:58:49 +0300

    Add 0 statement for factorial

--- squirrel.py
+++ squirrel.py
@@ -1,10 +1,13 @@
 def squirrel(x):
   diamonds = 0
-  def fact(x):
-    if(x == 1):
-      return 1
-    else:
-      return x * fact(x-1)
+  if x == 0:
+        return 1
+  else:
+    def fact(x):
+      if(x == 1):
+        return 1
+      else:
+        return x * fact(x-1)
+    diamonds = str(fact(x))[0]
+    return(int(diamonds))
   
-  diamonds = str(fact(x))[0]
-  return(int(diamonds))
