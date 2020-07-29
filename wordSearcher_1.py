47fe0679853b43484ab8d849e6f6e095622e2b17
antonkrupin <krupin_anton@mail.ru>
2020-07-29 14:20:23 +0300

    Update wordSearcher_1.py

--- wordSearcher_1.py
+++ wordSearcher_1.py
@@ -1,30 +1,38 @@
 def WordSearch(length, string, substring):
     splitString = []
-    subStringLength = 0
-    subs = ""
-    string = string.strip().split()
     
-    for x in range(len(string)):
-        m = len(string[x])
-        if(m > length):
-            splitString.append(string[x][:length])
-        if(m == length):
-            splitString.append(string[x])
-        if(m < length):
-            j = x + 1
-            test = []
-            test.append(x)
-            for j in range(len(string)):
-                m = m + len(string[j])
-                if(m == length):
-                    test.append(j)
-                    for i in range(len(test)):
-                        splitString.append(string[test[i]] + ' ')
-                if(m < length)
-        
+    pointer = 0
+    
+    if(length >= len(string)):
+        splitString.append(string)
+    else:
+        while(pointer <= len(string)):
+            m = string[pointer:pointer+length]
+            n = string[pointer:pointer+length+1]
+            if(' ' in m):
+                if(n[-1] == ' '):
+                    splitString.append(m)
+                    pointer = pointer + length
+                    #for i in reversed(range(len(m))):
+                        #if(m[i] == ' '):
+                            #splitString.append(m[:i])
+                            #pointer = pointer + i + 1
+                            #break
+                if(n[-1] != ' '):
+                    for i in reversed(range(len(m))):
+                        if(m[i] == ' '):
+                            splitString.append(m[:i])
+                            pointer = pointer + i + 1
+                            break
+                        
+            if(not ' ' in m):
+                splitString.append(m)
+                pointer = pointer + length    
+    
+    for x in range(len(splitString)-1):
+        if(splitString[x] == ''):
+            splitString.pop(x)
             
-    
-    return splitString
-    
-    
-print(WordSearch(12," строка разбивается на набор строк через выравнивание по заданной ширине.", "строк"))
+    print(splitString)
+        
+print(WordSearch(12,"строка разбивается на набор строк через выравнивание по заданной ширине.", "строк"))
