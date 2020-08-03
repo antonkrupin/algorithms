cc7c5f32b1c5534a1cad2e5c8823b56a27d0f6a7
antonkrupin <krupin_anton@mail.ru>
2020-08-03 15:25:59 +0300

    Add files via upload

---
+++ tankRush1.py
@@ -0,0 +1,37 @@
+def TankRush(h1,w1,map1,h2,w2,map2):
+    #h1, h2 - количество строк
+    #w1, w2 - количество столбцов
+    map1 = map1.split()
+    map2 = map2.split()
+    splittedMap = []
+    pointer = 0
+    crossingMaps = {}
+    
+    for x in range(len(map1)):
+        pointer = 0
+        sliceMap = []
+        for y in range(len(map1[x])):
+            mapSlice = map1[x][pointer:pointer+w2]
+            if(len(mapSlice) == w2):
+                sliceMap.append(mapSlice)
+            pointer += 1
+        splittedMap.append(sliceMap)
+    
+    for x in range(len(splittedMap)):
+        for y in range(len(splittedMap[x])):
+            for m in range(len(map2)):
+                cross = []
+                if(splittedMap[x][y] == map2[m]):
+                    #cross.append(map2[m])
+                    #cross.append(y)
+                    if(map2[m] in crossingMaps):
+                        test = crossingMaps[map2[m]]
+                        crossingMaps[map2[m]] = [test,y]
+                    else:
+                        crossingMaps[map2[m]] = y
+    
+    
+            
+    print(crossingMaps)
+         
+print(TankRush(3,4,'1234 2345 0987',2,2,'23 34 98'))
