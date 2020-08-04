3c62281c45f491f2ca850c17c30f76018f3fcd9c
antonkrupin <krupin_anton@mail.ru>
2020-08-04 16:04:24 +0300

    Update tankRush.py

--- tankRush.py
+++ tankRush.py
@@ -4,30 +4,35 @@ def TankRush(h1,w1,map1,h2,w2,map2):
     map1 = map1.split()
     map2 = map2.split()
     splittedMap = []
-    pointer = 0
     crossingMaps = []
+    positions = []
+    counters = []
+    strokes = []
+    testing = []
+    testing1 = []
+    if(h1 <= 0 or h2 <= 0 or w1 <= 0 or w2 <= 0):
+        return False
     
-    for x in range(len(map1)):
-        pointer = 0
-        sliceMap = []
-        for y in range(len(map1[x])):
-            mapSlice = map1[x][pointer:pointer+w2]
-            if(len(mapSlice) == w2):
-                sliceMap.append(mapSlice)
-            pointer += 1
-        splittedMap.append(sliceMap)
-    
-    for x in range(len(splittedMap)):
-        for y in range(len(splittedMap[x])):
-            for m in range(len(map2)):
-                cross = []
-                if(splittedMap[x][y] == map2[m]):
-                    cross.append(map2[m])
-                    cross.append(y)
-                    crossingMaps.append(cross)
-    
-    
-            
-    print(crossingMaps)
-         
-print(TankRush(3,4,'1234 2345 0987',2,2,'23 34 98'))
+    if(h2 > h1 or w2 > w1):
+        return False
+    else:
+        for x in range(len(map1)):
+            pointer = 0
+            for y in range(len(map1[x])):
+                if(len(map1[x][pointer:pointer+w2]) == w2):
+                    test = map1[x][pointer:pointer+w2]
+                    testing = []
+                    if(test == map2[0]):
+                        testing.append(1)
+                        for z in range(x+1,len(map1)):
+                            r = map1[z][pointer:pointer+w2]
+                            for i in range(1,len(map2)):
+                                if(r == map2[i]):
+                                    testing.append(1)
+                                else:
+                                    testing.append(0)
+                                    
+                    if(len(testing) != 0):
+                        testing1.append(testing)
+                pointer += 1
+        print(testing1)
