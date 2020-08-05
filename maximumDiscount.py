9678fbfcced9fb3d4060586b8f166bec682bace4
antonkrupin <krupin_anton@mail.ru>
2020-08-05 11:48:00 +0300

    Update maximumDiscount.py

--- maximumDiscount.py
+++ maximumDiscount.py
@@ -1,6 +1,14 @@
 def MaximumDiscount(quantityOfGoods, prices):
-    b = sorted(prices,reverse = True)
+    sortedPrices = sorted(prices)
+    baseDiscount = 0
     if(quantityOfGoods < 3):
         return 0
     if(quantityOfGoods == 3):
         return min(prices)
+        
+    def minPrice(prices):
+        return min(prices)
+        
+    if(quantityOfGoods > 3):
+        freeGoodsQuantity = len(prices)//3
+        baseDiscount = sum(sortedPrices[0:freeGoodsQuantity])
