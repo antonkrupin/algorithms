59a5ba90675087d5b648da7b3a3b2f40c79aa303
antonkrupin <krupin_anton@mail.ru>
2020-08-05 12:23:08 +0300

    Update maximumDiscount.py

--- maximumDiscount.py
+++ maximumDiscount.py
@@ -1,6 +1,7 @@
 def MaximumDiscount(quantityOfGoods, prices):
-    #sortedPrices = sorted(prices)
+    sortedPrices = sorted(prices)
     baseDiscount = 0
+    slicedPrices = []
     if(quantityOfGoods < 3):
         return 0
     if(quantityOfGoods == 3):
@@ -11,9 +12,23 @@ def MaximumDiscount(quantityOfGoods, prices):
         
     if(quantityOfGoods > 3):
         freeGoodsQuantity = len(prices)//3
-        baseDiscount = sum(prices[-freeGoodsQuantity:])
-        print(baseDiscount)
-        print(prices)
+        baseDiscount = sum(sortedPrices[:freeGoodsQuantity])
+        pointer = 0
         
+        for x in range(len(prices)):
+            p = prices[pointer:pointer+3]
+            if(len(p) == 3):
+                slicedPrices.append(prices[pointer:pointer+3])
+            pointer += 3
+            
+        discount = 0
+        
+        for x in range(len(slicedPrices)):
+            discount += min(slicedPrices[x])
+        
+        if(discount >= baseDiscount):
+            return discount
+        else:
+            return baseDiscount
 
-print(MaximumDiscount(7,[250,400,100,50,25,15,175]))
+print(MaximumDiscount(9,[250,400,50,100,25,175,15,85,500]))
