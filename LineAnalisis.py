4103041a1b4e21a91f7d47e4c1a65684627c0792
antonkrupin <krupin_anton@mail.ru>
2020-08-07 12:00:03 +0300

    Update LineAnalisis.py

--- LineAnalisis.py
+++ LineAnalisis.py
@@ -1,5 +1,6 @@
 def LineAnalysis(analyseString):
     pattern = ""
+    slicedString = []
     if(analyseString[0] == '*' and analyseString[-1] == '*'):
         for x in range(1,len(analyseString)):
             if(analyseString[x] == '*'):
@@ -11,14 +12,17 @@ def LineAnalysis(analyseString):
         
         for x in range(len(analyseString)):
             patternSlice = analyseString[pointer:pointer+p]
-            print(patternSlice)
-            if(len(patternSlice) == p):
-                if(patternSlice != pattern):
-                    return False
-            else:
-                return False
+            
+            if(patternSlice != '' and len(patternSlice) == p):
+                slicedString.append(patternSlice)
             pointer += p-1
+        
+        print(slicedString)
+        for x in range(len(slicedString)):
+            if(slicedString[x] != pattern):
+                return False
+                break
         return True
+        
     else:
         return False
-print(LineAnalysis('*..........*..........*'))
