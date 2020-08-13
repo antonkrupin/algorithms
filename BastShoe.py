59feb56785199e2bf7de8a4132ecfd07c8fd75db
antonkrupin <krupin_anton@mail.ru>
2020-08-13 16:09:23 +0300

    Update BastShoe.py

--- BastShoe.py
+++ BastShoe.py
@@ -70,7 +70,7 @@ def BastShoe(string):
         
         if(operationNumber == '3'):
             undoCounter[0] = True
-            if(int(endOfLine) < 0 or int(endOfLine) > len(fullString[0])):
+            if(int(endOfLine) < 0 or int(endOfLine) >= len(fullString[0])):
                 return ''
             else:
                 return fullString[0][int(endOfLine)]
