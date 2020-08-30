ece770f2f9667889c382027298974dee6a8309ae
antonkrupin <krupin_anton@mail.ru>
2020-08-30 13:35:08 +0300

    Update MatrixTurn.py

--- MatrixTurn.py
+++ MatrixTurn.py
@@ -1,6 +1,6 @@
 def MatrixTurn(matrix, matrixHeight, matrixLength, turns):
     martixArray = []
-    
+   
     if(matrixHeight >= matrixLength):
         numberOfCircles = matrixLength // 2
     else:
@@ -8,6 +8,7 @@ def MatrixTurn(matrix, matrixHeight, matrixLength, turns):
     
     for x in range(len(matrix)):
         martixArray.append(list(matrix[x]))
+
     turnCounter = 0
     
     while(turnCounter < turns):
@@ -37,8 +38,5 @@ def MatrixTurn(matrix, matrixHeight, matrixLength, turns):
                         martixArray[x-1].pop(-(i+1))
             i += 1
         turnCounter += 1
-    
-    for x in range(len(martixArray)):
-        martixArray[x] = ''.join(martixArray[x])
-
+        
     return martixArray   
