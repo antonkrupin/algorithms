1fd0d073396b6415109a8aca93953de13ad09db7
antonkrupin <krupin_anton@mail.ru>
2020-09-01 13:43:01 +0300

    Update TransformTransform.py

--- TransformTransform.py
+++ TransformTransform.py
@@ -1,22 +1,22 @@
 def TransformTransform(numbers, n):
     transformNumbers = []
     transformNumbers1 = []
-    
+
     for i in range(len(numbers)):
-        for j in range(0,len(numbers)-i-1):
+        for j in range(0,len(numbers)-i):
             k = i + j
-            if(len(numbers[j:k]) != 0):
-                maxNumber = max(numbers[j:k])
+            if(len(numbers[j:k+1]) != 0):
+                maxNumber = max(numbers[j:k+1])
                 transformNumbers.append(maxNumber)
     
     for i in range(len(transformNumbers)):
-        for j in range(0,len(transformNumbers)-i-1):
+        for j in range(0,len(transformNumbers)-i):
             k = i + j
-            if(len(transformNumbers[j:k]) != 0):
-                maxNumber = max(transformNumbers[j:k])
+            if(len(transformNumbers[j:k+1]) != 0):
+                maxNumber = max(transformNumbers[j:k+1])
                 transformNumbers1.append(maxNumber)
-    
-    if(sum(transformNumbers1)%2 == 0):
+
+    if(sum(transformNumbers1) % 2 == 0):
         return True
     else:
         return False
