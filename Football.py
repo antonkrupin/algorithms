a4b2e67f67103579e20054e9c122a45341434448
antonkrupin <krupin_anton@mail.ru>
2020-09-09 15:44:29 +0300

    Create Football.py

---
+++ Football.py
@@ -0,0 +1,41 @@
+def Football(numbers, n):
+    sortedNumbers = list(numbers)
+    sortedNumbers.sort()
+    
+    def flatten(lst):
+        while lst:
+            sublist = lst.pop(0)
+            if isinstance(sublist, list):
+                lst = sublist + lst
+            else:
+                yield sublist
+    
+    
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            numbersCopy = list(numbers)
+            if(i != j):
+                number1 = numbersCopy[i]
+                number2 = numbersCopy[j]
+                numbersCopy[i] = number2
+                numbersCopy[j] = number1
+            if(numbersCopy == sortedNumbers):
+                return True
+    
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            numbersCopy = list(numbers)
+            length = len(numbers)-j
+            numbersSlice1 = numbersCopy[0:i]
+            numbersSlice = numbersCopy[i:length]
+            reversedNumbersSlice = numbersSlice[::-1]
+            if(length <= 5):
+                if(len(numbersCopy[length:]) != 0):
+                    reversedNumbersSlice.append(numbersCopy[length:])
+                if(len(numbersSlice1) != 0):
+                    reversedNumbersSlice.insert(0,numbersSlice1)
+            
+            if(list(flatten(reversedNumbersSlice)) == sortedNumbers):
+                return True
+            
+    return False
