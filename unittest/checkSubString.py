3c8a03eea4411e84d77e43b06b94b542a33bca83
antonkrupin <krupin_anton@mail.ru>
2020-10-04 18:35:35 +0300

    Add files via upload

---
+++ unittest/checkSubString.py
@@ -0,0 +1,15 @@
+def checkSubString(string, subString):
+    if len(subString) == 0:
+        return True
+
+    for i in range(len(string)):
+        counter = 0
+        if string[i] == subString[0]:
+            counter += 1
+            for j in range(1,len(subString)):
+                if i + j < len(string):
+                    if string[i + j] == subString[j]:
+                        counter += 1
+        if counter == len(subString):
+            return True
+    return False
