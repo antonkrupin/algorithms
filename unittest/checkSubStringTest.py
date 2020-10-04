b3109715466049567521eecee7665dd9e36a61e4
antonkrupin <krupin_anton@mail.ru>
2020-10-04 18:35:02 +0300

    Add files via upload

---
+++ unittest/checkSubStringTest.py
@@ -0,0 +1,50 @@
+import unittest
+import string
+import random
+from random import randint
+from checkSubString import checkSubString as check
+
+class checkSubStringTests(unittest.TestCase):
+
+    def testRegression(self):
+        self.assertEqual(check('12345','234'),  True)
+        self.assertEqual(check('12345','2346'),False)
+        self.assertEqual(check('hello world', 'lo w'), True)
+        self.assertEqual(check('hello world', 'low'), False)
+
+    def testRandom(self):
+        for x in range(10000):
+            string1 = []
+            string2 = []
+
+            for x in range(1,10):
+                string1.append(randint(1,10))
+
+            string1 = ''.join(map(str,string1))
+
+            for x in range(4):
+                string2.append(randint(1,10))
+
+            string2 = ''.join(map(str,string2))
+
+            checkRandom = check(string1,string2)
+            self.assertEqual(checkRandom,string2 in string1)
+
+    def testNull(self):
+        self.assertEqual(check('','23'),False)
+        self.assertEqual(check('',''), True)
+        self.assertEqual(check('2345',''), True)
+
+    def testMax(self):
+
+        def generateString(size):
+            return ''.join(random.choice(string.ascii_letters) for _ in range(size))
+
+        string1 = generateString(1000000)
+        string2 = generateString(1000)
+
+        checkRandom = check(string1,string2)
+        self.assertEqual(checkRandom,string2 in string1)
+        
+if __name__ == 'main':
+    unittest.main()
