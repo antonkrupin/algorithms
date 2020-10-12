37abd3467a8eb206de57792cef2f73c2c9c7887d
antonkrupin <krupin_anton@mail.ru>
2020-10-12 19:05:13 +0300

    Add files via upload

---
+++ fileWorks/useFunctions.py
@@ -0,0 +1,4 @@
+from filesAndFoldersHandling import getDirectoryFilesAndFolders, deleteDirectory
+
+deleteDirectory('E:\\algorithms-master\\fileworks')
+getDirectoryFilesAndFolders('E:\\algorithms-master\\fileworks')
