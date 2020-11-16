cb9e0ea6e99839994459797b7106fbae89c218aa
antonkrupin <krupin_anton@mail.ru>
2020-11-16 16:02:20 +0300

    Update jsonFunctions.py

--- json/jsonFunctions.py
+++ json/jsonFunctions.py
@@ -1,7 +1,6 @@
 import json
 import requests
 
-
 def openJSON(link):
     response = requests.get(link)
     result = json.loads(response.text)
@@ -10,38 +9,48 @@ def openJSON(link):
 
 def saveJSONtoFile():
     data = openJSON("https://jsonplaceholder.typicode.com/todos")
-
     fo = open('dataJSON.json', 'wt')
     fo.write(json.dumps(data, indent=2))
     fo.close()
 
 
 def uniqueUsers():
-    with open('dataJSON.json', 'r') as f:
-        data = json.loads(f.read())
+    try:
+        with open('dataJSON.json', 'r') as f:
+            data = json.loads(f.read())
         
-    users = {}
+        users = {}
 
-    for item in data:
-        users[item['userId']] = item['userId']
+        for item in data:
+            users[item['userId']] = item['userId']
 
-    uniqueUsers = len(users.items())
+        uniqueUsers = len(users.items())
     
-    return uniqueUsers
+        return [uniqueUsers, 1]
 
+    except Exception:
+        uniqueUsers = {}
+        return [uniqueUsers, -1]
+        
 
 def originalTasks():
-    with open('dataJSON.json', 'r') as f:
-        data = json.loads(f.read())
+    try:
+        with open('dataJSON.json', 'r') as f:
+            data = json.loads(f.read())
 
-    usersTasks = {}
+        usersTasks = {}
 
-    for item in data:
-        usersTasks[item['userId']] = {'tasks':0, 'completedTasks':0}
-    
-    for item in data:
-        if item['completed'] == True:
-            usersTasks[item['userId']]['completedTasks'] += 1
-        usersTasks[item['userId']]['tasks'] += 1
-    
-    return usersTasks
+        for item in data:
+            usersTasks[item['userId']] = {'tasks':0, 'completedTasks':0}
+        
+        for item in data:
+            if item['completed'] == True:
+                usersTasks[item['userId']]['completedTasks'] += 1
+            usersTasks[item['userId']]['tasks'] += 1
+        
+        return [usersTasks, 1]
+
+    except Exception:
+        usersTasks = {}
+        return [usersTasks, -1]
+
