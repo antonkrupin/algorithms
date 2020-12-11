c9ab8b2a423f54b1c0cf04dd68fb90f1e907763f
antonkrupin <krupin_anton@mail.ru>
2020-12-11 15:36:39 +0300

    Add files via upload

---
+++ iterator/iteratorTest.py
@@ -0,0 +1,162 @@
+import unittest
+
+from random import randint
+from iterator import ListWithConstructor
+from iterator import ListWithConstructorInfinite
+
+
+class ListWithConstructorTest(unittest.TestCase):
+    def test_littleQuantityOfElements(self):
+        exampleSequence = [10,20,40,80,160,320,640,1280,2560]
+        resultSequence = []
+
+        for i in ListWithConstructor(10):
+            resultSequence.append(i)
+        
+        self.assertEqual(exampleSequence, resultSequence)
+
+    def test_randomElementsQuantity(self):
+        randomNumber = randint(10,100)
+        exampleSequence = []
+        resultSequence = []
+        
+        number = randomNumber
+        for i in range(1,10):
+            exampleSequence.append(number)
+            number = number*2
+
+        for i in ListWithConstructor(randomNumber):
+            resultSequence.append(i)
+
+        self.assertEqual(exampleSequence, resultSequence)
+
+    def test_randomElementsBigQuantity(self):
+        randomNumber = randint(10000, 1000000)
+        exampleSequence = []
+        resultSequence = []
+
+        number = randomNumber
+        for i in range(1, 10):
+            exampleSequence.append(number)
+            number = number * 2
+        
+        for i in ListWithConstructor(randomNumber):
+            resultSequence.append(i)
+
+        self.assertEqual(exampleSequence, resultSequence)
+
+    def test_cycleRandomElementsBigQuantity(self):
+        for i in range(1,5):
+            randomNumber = randint(10000, 1000000)
+            exampleSequence = []
+            resultSequence = []
+            number = randomNumber
+            for i in range(1, 10):
+                exampleSequence.append(number)
+                number = number * 2
+        
+            for i in ListWithConstructor(randomNumber):
+                resultSequence.append(i)
+
+            self.assertEqual(exampleSequence, resultSequence)
+
+
+class ListWithConstructorInfiniteTest(unittest.TestCase):
+    def test_littleQuantityOfElements(self):
+        exampleSequence = [1,2,3,4,5,6,7,8,9,10]
+        resultSequence = []
+
+        for i in ListWithConstructorInfinite(10, False):
+            resultSequence.append(i)
+        
+        self.assertEqual(exampleSequence, resultSequence)
+
+        resultSequence = []
+        counter = 0
+        for i in ListWithConstructorInfinite(10, True):
+            if counter < 10:
+                if len(resultSequence) != len(exampleSequence):
+                    resultSequence.append(i)
+                else:
+                    self.assertEqual(exampleSequence, resultSequence)
+                    counter += 1
+            else:
+                break
+
+    def test_randomElementsQuantity(self):
+        randomNumber = randint(10,100)
+        exampleSequence = []
+        resultSequence = []
+
+        for i in range(1,randomNumber+1):
+            exampleSequence.append(i)
+    
+        for i in ListWithConstructorInfinite(randomNumber, False):
+            resultSequence.append(i)
+
+        self.assertEqual(exampleSequence, resultSequence)
+    
+        resultSequence = []
+        counter = 0
+        for i in ListWithConstructorInfinite(randomNumber, True):
+            if counter < 10:
+                if len(resultSequence) != len(exampleSequence):
+                    resultSequence.append(i)
+                else:
+                    self.assertEqual(exampleSequence, resultSequence)
+                    counter += 1
+            else:
+                break
+
+    def test_randomElementsBigQuantity(self):
+        randomNumber = randint(10000, 1000000)
+        exampleSequence = []
+        resultSequence = []
+
+        for i in range(1, randomNumber+1):
+            exampleSequence.append(i)
+        
+        for i in ListWithConstructorInfinite(randomNumber, False):
+            resultSequence.append(i)
+
+        self.assertEqual(exampleSequence, resultSequence)
+
+        resultSequence = []
+        counter = 0
+        for i in ListWithConstructorInfinite(randomNumber, True):
+            if counter < 10:
+                if len(resultSequence) != len(exampleSequence):
+                    resultSequence.append(i)
+                else:
+                    self.assertEqual(exampleSequence, resultSequence)
+                    counter += 1
+            else:
+                break
+    
+    def test_cycleRandomElementsBigQuantity(self):
+        for i in range(1,5):
+            randomNumber = randint(10000, 1000000)
+            exampleSequence = []
+            resultSequence = []
+            for i in range(1, randomNumber+1):
+                exampleSequence.append(i)
+        
+            for i in ListWithConstructorInfinite(randomNumber, False):
+                resultSequence.append(i)
+
+            self.assertEqual(exampleSequence, resultSequence)
+
+            resultSequence = []
+            counter = 0
+            for i in ListWithConstructorInfinite(randomNumber, True):
+                if counter < 10:
+                    if len(resultSequence) != len(exampleSequence):
+                        resultSequence.append(i)
+                    else:
+                        self.assertEqual(exampleSequence, resultSequence)
+                        counter += 1
+                else:
+                    break
+
+if __name__ == '__main__':
+    unittest.main()
