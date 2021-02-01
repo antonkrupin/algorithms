bdb06741c28eb23de64fd941cb5895921b8984d3
antonkrupin <krupin_anton@mail.ru>
2021-02-01 13:33:37 +0300

    Add files via upload

--- algorithms_part1/linkedList.py
+++ algorithms_part1/linkedList.py
@@ -1,13 +1,7 @@
-test = [2,13,2,4,35,13,99,18,13,123,2,1,10,15,6]
-
 class Node:
     def __init__(self, v):
         self.value = v
         self.next = None
-
-n1 = Node(12)
-n2 = Node(55)
-n1.next = n2
 
 class LinkedList:
     def __init__(self):
@@ -60,10 +54,8 @@ class LinkedList:
             if headNode is not None:
                 if headNode.value == val:
                     self.head = headNode.next
-                    #headNode = None
                     prevNode = headNode
                     headNode = headNode.next
-                    #return
             
             while headNode is not None:
                 if headNode.value == val:
@@ -74,6 +66,21 @@ class LinkedList:
             if headNode == None:
                 return
 
+    def clean(self):
+        self.__init__()
+        
+    def find_all(self, val):
+        headNode = self.head
+        nodesList = []
+        while headNode is not None:
+            if headNode.value == val:
+                nodesList.append(headNode)
+            headNode = headNode.next
+        if nodesList != []:
+            return nodesList
+        else:
+            return None
+
     def len(self):
         length = 0
         node = self.head
@@ -82,18 +89,18 @@ class LinkedList:
             node = node.next
         return length
 
-#[2,13,2,4,35,13,99,18,13,123,2,1,10,15,6]
-s_list = LinkedList()
+    def insert(self, afterNode, newNode):
+        startNode = self.head
+        
+        if afterNode == None:
+            self.head = Node(newNode)
+            self.head.next = startNode
+            return 
 
-for i in test:
-    s_list.add_in_tail(Node(i))
-
-s_list.print_all_nodes()
-print('____')
-s_list.delete(6, all=True)
-s_list.print_all_nodes()
-#s_list.delete(13, all=True)
-#print('______')
-#print(s_list.len())
-#print('_____')
-#s_list.print_all_nodes()
+        while startNode is not None:
+            if startNode.value == afterNode:
+                nodeForAppend = Node(newNode)
+                nextNodeLink = startNode.next
+                nodeForAppend.next = nextNodeLink
+                startNode.next = nodeForAppend
+            startNode = startNode.next
