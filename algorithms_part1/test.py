af67ef8dff68ddd7dc877a2281e7fe19260936a2
antonkrupin <krupin_anton@mail.ru>
2021-02-11 16:47:44 +0300

    Create test.py

---
+++ algorithms_part1/test.py
@@ -0,0 +1,86 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+class LinkedList2:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+        self.tail = item
+       
+    def print_all_nodes(self):
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+    
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        node = self.head
+        nodesList = []
+        while node is not None:
+            if node.value == val:
+                nodesList.append(node)
+            node = node.next
+
+        return nodesList
+    
+    def delete(self, val, all=False):
+        node = self.head
+        if all == False:
+            while node is not None:
+                if node.value == val:
+                    if node.prev == None:
+                        if self.head.next == None:
+                            self.head == None
+                            self.tail == None
+                        else:
+                            self.head.next.prev = None
+                            self.head = self.head.next
+                    else:
+                        if node.next == None:
+                            node.prev.next = None
+                            self.tail = node.prev
+                        else:
+                            node.prev.next = node.next
+                            node.next.prev = node.prev
+                    return True
+                else:
+                    node = node.next
+            return False
+        else:
+            while node is not None:
+                if node.value == val:
+                    if node.prev == None:
+                        if self.head.next == None:
+                            self.head = None
+                            self.tail = None
+                        else:
+                            self.head.next.prev = None
+                            self.head = self.head.next
+                    else:
+                        if node.next == None:
+                            node.prev.next = None
+                            self.tail = node.prev
+                        else:
+                            node.prev.next = node.next
+                            node.next.prev = node.prev
+                node = node.next
