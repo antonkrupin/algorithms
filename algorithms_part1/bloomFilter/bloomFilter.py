92d34b6e48b4b410472f00c2e3625ce3c1473e78
antonkrupin <krupin_anton@mail.ru>
2021-04-13 18:47:38 +0300

    Add files via upload

--- algorithms_part1/bloomFilter/bloomFilter.py
+++ algorithms_part1/bloomFilter/bloomFilter.py
@@ -2,9 +2,11 @@ class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
-        #self.bitArray = [0] * self.filter_len
-        self.bitArray = bin(2 ** self.filter_len)[3:]
+        #self.bitArray = self.set_bit(0b00000000000000000000000000000000, self.filter_len)
+        self.bitArray = 0b00000000000000000000000000000000 | (1 << self.filter_len)
 
+    def set_bit(self, A,k):
+        return A | (1 << k)
 
     def hash1(self, str1):
         res = 0
@@ -12,8 +14,7 @@ class BloomFilter:
             code = ord(c)
             res = (res * 17 + code) % 32
         return res
-        # реализация ...
-
+        
     def hash2(self, str1):
         res = 0
         for c in str1:
@@ -22,16 +23,27 @@ class BloomFilter:
         return res
 
     def add(self, str1):
-        """
         h1 = self.hash1(str1)
         h2 = self.hash2(str1)
-        b1 = bin(h1)
-        b2 = bin(h2)
-        self.bitArray
-        """
-        pass
+        self.bitArray = self.bitArray | h1
+        self.bitArray = self.bitArray | h2
 
     def is_value(self, str1):
-        # проверка, имеется ли строка str1 в фильтре
-        pass
+        h1 = self.hash1(str1)
+        h2 = self.hash2(str1)
+        #temp = 0b00000000000000000000000000000000 | (1 << self.filter_len)
+        temp = self.bitArray
+        test = temp | h1
+        #print('temp | h1', bin(temp | h1))
+        test1 = test | h2
+        #print('temp | h2', bin(temp | h2))
 
+        tempResultArray = test1
+        tempResultArrayString = bin(tempResultArray)
+
+        originalArray = bin(self.bitArray)
+
+        for i in range(self.filter_len):
+            if tempResultArrayString[i+3] != originalArray[i+3]:
+                return False
+        return True
