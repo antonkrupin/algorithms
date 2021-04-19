ead3280b0fc404b1b12f5ee867e9745988d8fca3
antonkrupin <krupin_anton@mail.ru>
2021-04-19 16:11:53 +0300

    Add files via upload

---
+++ algorithms_part1/cache/cache.py
@@ -0,0 +1,41 @@
+class NativeCache:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+        self.hits = [0] * self.size
+
+    def hash_fun(self, key):
+        sum = 0
+        for i in range(len(key)):
+            sum += 1
+        return sum % self.size
+
+    def is_key(self, key):
+        for i in range(len(self.values)):
+            if self.values[i] == key:
+                return True
+        return False
+
+    def put(self, key, value):
+        index = self.hash_fun(key)
+        self.values[index] = key
+        self.slots[index] = value
+
+    def get(self, key):
+        if self.is_key(key):
+            index = self.hash_fun(key)
+            self.hits[index] = self.hits[index] + 1
+            return self.slots[self.hash_fun(key)]
+        else:
+            return None
+
+nc = NativeCache(32)
+
+
+nc.put('name', 'Anton')
+print(nc.slots)
+print(nc.values)
+nc.get('name')
+nc.get('name')
+print(nc.hits)
